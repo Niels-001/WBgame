39ede80c584c7d7c9ad686e5b5277446efe80cc5
kobus <kobushendrikus@gmail.com>
2023-09-27 17:33:19 +0200

    kobus commit initial

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("hallo world")
-print("HALLO")
-print("by world")
+def je_moeder ():
+    if input("Wie wordt er geneukt? Mijn...") == "Moeder" or "moeder":
+        print("Daar heb je helemaal gelijk in")
+    else:
+        print("Nope, probeer het eens opnieuw.")
+        je_moeder()
 
+je_moeder()
