a20c3a6c4241336999ac0358e6a0f2353d2e5160
NvGil <nvgilpen@hotmail.com>
2023-10-10 16:33:33 +0200

    Basic Game structure

--- main.py
+++ main.py
@@ -1,10 +1,49 @@
-def je_moeder ():
-    moeder = input("Wie wordt er geneukt? Mijn...")
-    if  moeder in ["moeder","Moeder"]:
-        print("Daar heb je helemaal gelijk in")
-    else:
-        print("Nope, probeer het eens opnieuw.")
-        je_moeder()
+'''Imports of important python libraries
+'''
+import pygame   #for the UI, game mechanics and event handeling
+import time     #for all time related commands
 
-while True:
-    je_moeder()
+pygame.init()   #initiate pygame
+
+'''GAME Window
+'''
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+'''game variables, and functions'''
+player = pygame.Rect((300,250,20,20))
+
+'''GAME Loop
+'''
+run = True
+while run:
+    time.sleep(0.001)
+
+    '''Draw playing field & assets'''
+    screen.fill((0,0,0))
+
+    '''Draw players & NPC's'''
+    pygame.draw.rect(screen, (255, 255, 255), player)
+
+    '''GAME Event Handler
+    '''
+    key = pygame.key.get_pressed()
+    if key[pygame.K_a] == True:
+        player.move_ip(-1,0)
+    elif key[pygame.K_d] == True:
+        player.move_ip(1,0)
+    elif key[pygame.K_s] == True:
+        player.move_ip(0, 1)
+    elif key[pygame.K_w] == True:
+        player.move_ip(0, -1)
+
+    '''GAME menu & quit'''
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
