f8512a6674f63060a23279afbdde47dfb5cdce3d
kobus <kobushendrikus@gmail.com>
2023-10-12 22:09:09 +0200

    Iets netter gemaakt en een lijn in het midden gezet

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-'''Imports of important python libraries
-'''
+# Imports of important python libraries
 import pygame   # for the UI, game mechanics and event handling
 import time     # for all time related commands
 import Object_setup as Ob
@@ -10,8 +9,7 @@ pygame.init()   # initiate pygame
 clock = pygame.time.Clock()
 FPS = 60
 
-'''GAME Window
-'''
+# Game window
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
@@ -23,8 +21,8 @@ pygame.display.set_caption("BoxHead")
 Characters = pygame.sprite.Group()
 
 # create square and add to squares group
-player_1 = Ob.Character("Blue", 500, 300)
-player_2 = Ob.Character("Red", 300, 300)
+player_1 = Ob.Character("Blue", 300, 300)
+player_2 = Ob.Character("Red", 500, 300)
 
 Characters.add(player_1, player_2)
 
@@ -37,11 +35,12 @@ while run:
 
     '''Draw players & NPC's'''
     Characters.update()
+    pygame.draw.line(screen, (0, 0, 0), (SCREEN_WIDTH / 2, 0), (SCREEN_WIDTH/2, SCREEN_HEIGHT), 3)
     Characters.draw(screen)
 
     '''GAME Event Handler'''
 
-    Ob.Character.inputs(player_1,pygame.K_w,pygame.K_s,pygame.K_a,pygame.K_d)
+    Ob.Character.inputs(player_1, pygame.K_w, pygame.K_s, pygame.K_a, pygame.K_d)
     Ob.Character.inputs(player_2, pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT)
 
     '''GAME menu & quit'''
