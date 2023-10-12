19612b1c38a654ce757c557cd4c8f4c1b2f0685b
kobus <kobushendrikus@gmail.com>
2023-10-12 19:55:29 +0200

    Kobus die inputs in een functie in de setup file heeft gezet

--- Object_setup.py
+++ Object_setup.py
@@ -1,17 +1,56 @@
 import pygame
 import math
 
-class Character(pygame.sprite.Sprite):
-  def __init__(self, col, x, y):
-    pygame.sprite.Sprite.__init__(self)
-    self.image = pygame.Surface((30, 30))
-    self.image.fill(col)
-    self.rect = self.image.get_rect()
-    self.rect.center = (x, y)
-    self.ms = 5
 
-  def move(self, x, y):
-    if abs(x) == abs(y):
-      self.rect.move_ip(x * self.ms, y * self.ms)
-    else:
-      self.rect.move_ip(x*self.ms * math.sqrt(2), y*self.ms * math.sqrt(2))
+class Character(pygame.sprite.Sprite):
+    def __init__(self, col, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 30))
+        self.image.fill(col)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.ms = 5
+
+    def move(self, x, y):
+        if abs(x) == abs(y):
+            self.rect.move_ip(x * self.ms, y * self.ms)
+        else:
+            self.rect.move_ip(x * self.ms * math.sqrt(2), y * self.ms * math.sqrt(2))
+
+    def inputs(self, up, down, left, right):
+        key = pygame.key.get_pressed()
+        # print(key[pygame.K_a], key[pygame.K_d], key[pygame.K_w], key[pygame.K_s])
+
+        if key[left] * key[down] * key[up] * key[down]:
+            self.move(0, 0)
+        # 3 keys pressed
+        elif key[left] * key[right] * key[up]:
+            self.move(0, -1)
+        elif key[left] * key[right] * key[down]:
+            self.move(0, 1)
+        elif key[left] * key[up] * key[down]:
+            self.move(-1, 0)
+        elif key[right] * key[up] * key[down]:
+            self.move(1, 0)
+        # 2 keys pressed
+        elif key[left] and key[up]:
+            self.move(-1, -1)
+        elif key[left] * key[right]:
+            self.move(0, 0)
+        elif key[left] * key[down]:
+            self.move(-1, 1)
+        elif key[right] * key[up]:
+            self.move(1, -1)
+        elif key[right] * key[down]:
+            self.move(1, 1)
+        elif key[up] * key[down]:
+            self.move(0, 0)
+        # 1 key pressed
+        elif key[left]:
+            self.move(-1, 0)
+        elif key[right]:
+            self.move(1, 0)
+        elif key[up]:
+            self.move(0, -1)
+        elif key[down]:
+            self.move(0, 1)
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 '''Imports of important python libraries
 '''
-import pygame   #for the UI, game mechanics and event handeling
-import time     #for all time related commands
+import pygame   # for the UI, game mechanics and event handling
+import time     # for all time related commands
 import Object_setup as Ob
 
+pygame.init()   # initiate pygame
 
-
-pygame.init()   #initiate pygame
-
-#frame rate
+# frame rate
 clock = pygame.time.Clock()
 FPS = 60
 
@@ -21,71 +19,30 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("BoxHead")
 
 '''game variables, and functions'''
-#create sprite group for squares
+# create sprite group for squares
 Characters = pygame.sprite.Group()
 
-#create square and add to squares group
+# create square and add to squares group
 player_1 = Ob.Character("Blue", 500, 300)
 player_2 = Ob.Character("Red", 300, 300)
 
 Characters.add(player_1, player_2)
 
-
-'''GAME Loop
-'''
+# Game Loop
 run = True
 while run:
     clock.tick(FPS)
     screen.fill("beige")
     '''Draw playing field & assets'''
 
-
     '''Draw players & NPC's'''
     Characters.update()
     Characters.draw(screen)
 
-    '''GAME Event Handler
-    '''
-    key = pygame.key.get_pressed()
-    #movement controls player 1
-    # 4 keys pressed
-    # print(key[pygame.K_a], key[pygame.K_d], key[pygame.K_w], key[pygame.K_s])
+    '''GAME Event Handler'''
 
-    if key[pygame.K_a] * key[pygame.K_d] * key[pygame.K_w] * key[pygame.K_s] :
-        player_1.move(0,0)
-    # 3 keys pressed
-    elif key[pygame.K_a] * key[pygame.K_d] * key[pygame.K_w] :
-        player_1.move(0,-1)
-    elif key[pygame.K_a] * key[pygame.K_d] * key[pygame.K_s] :
-        player_1.move(0, 1)
-    elif key[pygame.K_a] * key[pygame.K_w] * key[pygame.K_s] :
-        player_1.move(-1, 0)
-    elif key[pygame.K_d] * key[pygame.K_w] * key[pygame.K_s] :
-        player_1.move(1, 0)
-    # 2 keys pressed
-    elif key[pygame.K_a] and key[pygame.K_w]:
-        player_1.move(-1, -1)
-    elif key[pygame.K_a] * key[pygame.K_d] :
-        player_1.move(0, 0)
-    elif key[pygame.K_a] * key[pygame.K_s] :
-        player_1.move(-1, 1)
-    elif key[pygame.K_d] * key[pygame.K_w] :
-        player_1.move(1, -1)
-    elif key[pygame.K_d] * key[pygame.K_s] :
-        player_1.move(1, 1)
-    elif key[pygame.K_w] * key[pygame.K_s] :
-        player_1.move(0, 0)
-    # 1 key pressed
-    elif key[pygame.K_a] :
-        player_1.move(-1, 0)
-    elif key[pygame.K_d] :
-        player_1.move(1, 0)
-    elif key[pygame.K_w] :
-        player_1.move(0, -1)
-    elif key[pygame.K_s]:
-        player_1.move(0, 1)
-
-
+    Ob.Character.inputs(player_1,pygame.K_w,pygame.K_s,pygame.K_a,pygame.K_d)
+    Ob.Character.inputs(player_2, pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT)
 
     '''GAME menu & quit'''
     for event in pygame.event.get():
